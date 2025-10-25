2cc116e13f3dd7edc2ce7390f3ecd83b162cd8c9
Noé Gnanih <noegnanih@gmail.com>
2025-10-25 14:29:01 +0000

    feat: update admin panel

--- creopse/assets/plugin-D58OOUud.js
+++
@@ -1,1 +0,0 @@
-import{aE as e}from"./main-BFQh8EtE.js";const s=()=>{const a=e(),t=u=>a.data.value?.find(n=>n.id===u);return{pluginsData:a.data,pluginQuery:a,getPluginData:t}};export{s as u};
---
+++ creopse/assets/plugin-DpdJHsT_.js
@@ -0,0 +1,1 @@
+import{aF as e}from"./main-D6UvRL7q.js";const s=()=>{const a=e(),t=u=>a.data.value?.find(n=>n.id===u);return{pluginsData:a.data,pluginQuery:a,getPluginData:t}};export{s as u};
