0590da3a8a605ae74ca64e739e97d32fe13d13b5
Noé Gnanih <noegnanih@gmail.com>
2025-11-28 23:38:04 +0000

    chore: upgrade admin panel

--- creopse/assets/plugin-9lx-Cbvw.js
+++
@@ -1,1 +0,0 @@
-import{aF as e}from"./main-DumBU3o2.js";const s=()=>{const a=e(),t=u=>a.data.value?.find(n=>n.id===u);return{pluginsData:a.data,pluginQuery:a,getPluginData:t}};export{s as u};
---
+++ creopse/assets/plugin-A1_6amL9.js
@@ -0,0 +1,1 @@
+import{aF as e}from"./main-BRU0R4jH.js";const s=()=>{const a=e(),t=u=>a.data.value?.find(n=>n.id===u);return{pluginsData:a.data,pluginQuery:a,getPluginData:t}};export{s as u};
