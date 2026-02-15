38e57603ff91033fd9d1c90665a06c6e94acc0ca
Noé Gnanih <noegnanih@gmail.com>
2026-02-15 21:54:28 +0000

    chore: update admin panel

---
+++ creopse/assets/plugin-Y9QHKB0F.js
@@ -0,0 +1,1 @@
+import{aE as e}from"./main-Bt5KBR9p.js";const s=()=>{const a=e(),t=u=>a.data.value?.find(n=>n.id===u);return{pluginsData:a.data,pluginQuery:a,getPluginData:t}};export{s as u};
--- creopse/assets/profile-type-BLRZpE3c.js
+++
@@ -1,1 +0,0 @@
-import{i,P as r}from"./main-BjfMTYmS.js";import{u as n}from"./vue-i18n-ayrbyXZa.js";const m=()=>{const{t:e}=n(),{getSettingValue:a}=i(),t=[{label:a("adminProfileTypeLabel")||e("admin"),value:r.ADMIN}];return{profileTypes:t,getProfileTypeLabel:l=>t.find(o=>o.value===l)?.label||e("other")}};export{m as u};
